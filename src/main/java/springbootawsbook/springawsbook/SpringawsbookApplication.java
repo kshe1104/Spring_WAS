109097a8ef1152b778db3ff33b7d181a0098702f
taejin <kshe3768@gmail.com>
2025-01-26 22:27:59 +0900

    Initial commit

---
+++ src/main/java/springbootawsbook/springawsbook/SpringawsbookApplication.java
@@ -0,0 +1,13 @@
+package springbootawsbook.springawsbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringawsbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringawsbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springbootawsbook/springawsbook/SpringawsbookApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootawsbook.springawsbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringawsbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
