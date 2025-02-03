6670a5ebf9a142af87adf89c67a67e4eaa34a329
taejin <kshe3768@gmail.com>
2025-02-03 15:00:51 +0900

    SpringDataJPA에서 제공하지 않는 쿼리를 Query로 직접 추가

--- src/main/java/springbootawsbook/springawsbook/domain/posts/PostsRepository.java
+++ src/main/java/springbootawsbook/springawsbook/domain/posts/PostsRepository.java
@@ -1,7 +1,14 @@
 package springbootawsbook.springawsbook.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 
+    //SpringDataJPA에서 제공하지 않는 메소드 -> 쿼리로 작성
+    //Query는 가독성이 좋음
 }
